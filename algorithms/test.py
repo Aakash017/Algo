f73843e747356ccd2ee57c3052942d138cdb079c
aakashsharma007 <aakash.sharma@pasarpolis.com>
2023-04-24 11:52:05 +0530

    changes

---
+++ algorithms/test.py
@@ -0,0 +1,26 @@
+# 1-10
+
+
+def r(n):
+    print(n)
+    n=n+1
+    if n<11:
+        r(n)
+    return True
+print(r(1))
+
+# Find missing element
+n = 5
+l = [2,5,1,3]
+sum=0
+for i in range(1,len(l)):
+    sum+=i
+
+sum=15
+l=[11]
+
+
+
+'select count(employees) from employee where dept_id=1'
+'select max(salary) from employee table where dept_id=1'
+'select distinct salary from employee order by salary desc limit 3 , 1'
